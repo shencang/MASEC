68f6677cd3921823598720100fa3b3cea5c11fab
shencang <2630610009@qq.com>
2019-05-18 23:16:07 +0800

    更新项目

--- RecyclerViewDemo/app/src/main/java/com/shencangblue/jin/recyclerviewdemo/FruitAdapter.java
+++ RecyclerViewDemo/app/src/main/java/com/shencangblue/jin/recyclerviewdemo/FruitAdapter.java
@@ -7,18 +7,21 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
 public class FruitAdapter extends RecyclerView.Adapter<FruitAdapter.MyViewHolder> {
     List<Fruit> fruits = null;
     static class MyViewHolder extends RecyclerView.ViewHolder{
+        View fruitView ;
 
         private ImageView fruit_image;
         private TextView  fruit_namre;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
+            fruitView= itemView;
             fruit_image  = (ImageView)itemView.findViewById(R.id.fruit_Image_view);
             fruit_namre  = (TextView)itemView.findViewById(R.id.fruit_name);
         }
@@ -37,15 +40,32 @@ public class FruitAdapter extends RecyclerView.Adapter<FruitAdapter.MyViewHolder
                 .from(viewGroup.getContext())
                 .inflate(R.layout.fruit_item,viewGroup,false);
 
-        return new MyViewHolder(view);
+        final MyViewHolder myViewHolder = new MyViewHolder(view);
+        myViewHolder.fruitView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int position = myViewHolder.getAdapterPosition();
+                Fruit fruit = fruits.get(position);
+                Toast.makeText(v.getContext(),fruit.getName(),Toast.LENGTH_LONG).show();
+
+            }
+        });
+        myViewHolder.fruit_image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int position = myViewHolder.getAdapterPosition();
+                Fruit fruit = fruits.get(position);
+                Toast.makeText(v.getContext(),fruit.getName()+"的照片",Toast.LENGTH_LONG).show();
+            }
+        });
+        return myViewHolder;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MyViewHolder myViewHolder, int i) {
+    public void onBindViewHolder(@NonNull  MyViewHolder myViewHolder, int i) {
         Fruit fruit  = fruits.get(i);
         myViewHolder.fruit_image.setImageResource(fruit.getImageId());
         myViewHolder.fruit_namre.setText(fruit.getName());
-
 
     }
 
--- RecyclerViewDemo/app/src/main/java/com/shencangblue/jin/recyclerviewdemo/MainActivity.java
+++ RecyclerViewDemo/app/src/main/java/com/shencangblue/jin/recyclerviewdemo/MainActivity.java
@@ -1,12 +1,18 @@
 package com.shencangblue.jin.recyclerviewdemo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     private List<Fruit>fruits = new ArrayList<>();
@@ -18,29 +24,85 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         recyclerView = (RecyclerView)findViewById(R.id.recycler_view);
 
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(linearLayoutManager);
+
+//        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+//        linearLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+        StaggeredGridLayoutManager setLayoutManager=
+                new StaggeredGridLayoutManager(4,StaggeredGridLayoutManager.VERTICAL) ;
+        recyclerView.setLayoutManager(setLayoutManager);
+        //recyclerView.setLayoutManager(linearLayoutManager);
         FruitAdapter fruitAdapter = new FruitAdapter(fruits);
         recyclerView.setAdapter(fruitAdapter);
-        initFruit();
+        //initFruit();
+        initData();
+
+    }
+    private void initData(){
+        String []name = new String[]{"apple","banana","cherry"
+                                        ,"grape","mango","orange",
+                "pear","pineapple","strawberry","watermelon"};
+        int []img= {R.drawable.apple_pic,R.drawable.banana_pic,R.drawable.cherry_pic,R.drawable.grape_pic,
+                R.drawable.mango_pic,R.drawable.orange_pic,R.drawable.pear_pic,R.drawable.pineapple_pic,
+                R.drawable.strawberry_pic,R.drawable.watermelon_pic};
+        Random random =new Random();
+
+        int count = 0;
+        while (count<200){
+            int length =random.nextInt(10);
+            fruits.add(new Fruit(getRandomLengthName(name[length]),img[length]));
+            count++;
+        }
+
 
     }
 
     private  void initFruit(){
-        for (int i=0;i<2;i++){
-            fruits.add(new Fruit("apple",R.drawable.apple_pic));
-            fruits.add(new Fruit("banana",R.drawable.banana_pic));
-            fruits.add(new Fruit("cherry",R.drawable.cherry_pic));
-            fruits.add(new Fruit("grape",R.drawable.grape_pic));
-            fruits.add(new Fruit("mango",R.drawable.mango_pic));
-            fruits.add(new Fruit("orange",R.drawable.orange_pic));
-            fruits.add(new Fruit("pear",R.drawable.pear_pic));
-            fruits.add(new Fruit("pineapple",R.drawable.pineapple_pic));
-            fruits.add(new Fruit("strawberry",R.drawable.strawberry_pic));
-            fruits.add(new Fruit("watermelon",R.drawable.watermelon_pic));
+        for (int i=0;i<10;i++){
+            fruits.add(new Fruit(getRandomLengthName("apple"),R.drawable.apple_pic));
+            fruits.add(new Fruit(getRandomLengthName("banana"),R.drawable.banana_pic));
+            fruits.add(new Fruit(getRandomLengthName("cherry"),R.drawable.cherry_pic));
+            fruits.add(new Fruit(getRandomLengthName("grape"),R.drawable.grape_pic));
+            fruits.add(new Fruit(getRandomLengthName("mango"),R.drawable.mango_pic));
+            fruits.add(new Fruit(getRandomLengthName("orange"),R.drawable.orange_pic));
+            fruits.add(new Fruit(getRandomLengthName("pear"),R.drawable.pear_pic));
+            fruits.add(new Fruit(getRandomLengthName("pineapple"),R.drawable.pineapple_pic));
+            fruits.add(new Fruit(getRandomLengthName("strawberry"),R.drawable.strawberry_pic));
+            fruits.add(new Fruit(getRandomLengthName("watermelon"),R.drawable.watermelon_pic));
 
         }
 
+
+
+    }
+    private String getRandomLengthName(String name){
+        Random random =new Random();
+        int length =random.nextInt(20)+1;
+        StringBuilder builder  =new StringBuilder();
+        for (int i =0;i<length;i++){
+            builder.append(name);
+        }
+        return builder.toString();
+
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.log_out:{
+                Toast.makeText(this,"你想注销.",Toast.LENGTH_LONG).show();
+                break;
+            }
+            case R.id.exit:{Toast.makeText(this,"你想退出.",Toast.LENGTH_LONG).show();
+                break;}
+            default:{break;}
+        }
+
+        return  super.onOptionsItemSelected(item);
+    }
 }
